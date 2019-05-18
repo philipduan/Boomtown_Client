bc35cbe2bb018106fa1fe8935555e0ee53e95a17
philip du <philipdu@philips-MacBook-Pro.local>
2019-05-18 12:03:34 -0700

    Testing

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
-import HeaderBar from '../HeaderBar/HeaderBar';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
-
-import './styles.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import HeaderBar from "../HeaderBar/HeaderBar";
+import FloatingActionButton from "material-ui/FloatingActionButton";
+import ContentAdd from "material-ui/svg-icons/content/add";
+//testing commit
+import "./styles.css";
 
 const Layout = ({ children, location, history }) => {
   return (
     <div className="appContentWrapper">
       <div className="appHeader">
-        {location.pathname !== '/' ? <HeaderBar /> : null}
+        {location.pathname !== "/" ? <HeaderBar /> : null}
       </div>
-      {location.pathname === '/items' || location.pathname.includes('profile') ? (
+      {location.pathname === "/items" ||
+      location.pathname.includes("profile") ? (
         <FloatingActionButton
           style={{
-            position: 'fixed',
-            right: '20px',
-            bottom: '20px',
-            zIndex: '2'
+            position: "fixed",
+            right: "20px",
+            bottom: "20px",
+            zIndex: "2"
           }}
-          backgroundColor={'black'}
-          onClick={() => history.push('/share')}
+          backgroundColor={"black"}
+          onClick={() => history.push("/share")}
         >
           <ContentAdd />
         </FloatingActionButton>
