0cc3edcbdf9736d098946b3cb0049ae9bb72cc79
philip <philip_du_an@hotmail.com>
2019-05-18 13:47:54 -0700

    Some more testing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import registerServiceWorker from "./registerServiceWorker";
-//testing
+//testinx
 import "./index.css";
 import muiTheme from "./config/theme";
 
